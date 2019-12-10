62ca4c06b22e6f327e772266235e7345bc9c097e
Alejandro50666 <54365068+Alejandro50666@users.noreply.github.com>
2019-12-09 21:02:56 -0600

    Add files via upload

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,47 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Marisool",
+      message: "Hola, amigo! Vamos a una fiesta",
+      time: "18:30",
+      avatarUrl:
+          "https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/06/22/Recortada/img_melies_20180622-155238_imagenes_lv_terceros_emoyis-knNG-U453244591237BD-992x558@LaVanguardia-Web.JPG"),
+  new ChatModel(
+      name: "Eduardo",
+      message: "Te veo alrrato",
+      time: "17:30",
+      avatarUrl:
+          "https://imagenes.milenio.com/ayNqhD93oWGFJMKyV7GzAO5DQxA=/958x596/smart/https://www.milenio.com/uploads/media/2019/07/23/te-decimos-el-significado-verdadero_0_6_614_382.png"),
+  new ChatModel(
+      name: "Gregorio",
+      message: "Que onda!",
+      time: "10:00",
+      avatarUrl:
+          "https://imagenes.milenio.com/ayNqhD93oWGFJMKyV7GzAO5DQxA=/958x596/smart/https://www.milenio.com/uploads/media/2019/07/23/te-decimos-el-significado-verdadero_0_6_614_382.png"),
+  new ChatModel(
+      name: "Lalo",
+      message: "Llegue bien, gracias amigo",
+      time: "10:30",
+      avatarUrl:
+          "https://imagenes.milenio.com/ayNqhD93oWGFJMKyV7GzAO5DQxA=/958x596/smart/https://www.milenio.com/uploads/media/2019/07/23/te-decimos-el-significado-verdadero_0_6_614_382.png"),
+  new ChatModel(
+      name: "Clauddia",
+      message: "No entrare a clase jaja",
+      time: "12:30",
+      avatarUrl:
+          "https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/06/22/Recortada/img_melies_20180622-155238_imagenes_lv_terceros_emoyis-knNG-U453244591237BD-992x558@LaVanguardia-Web.JPG"),
+  new ChatModel(
+      name: "Juan",
+      message: "Ya termine el trabajo de Limon",
+      time: "6:50",
+      avatarUrl:
+          "https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/06/22/Recortada/img_melies_20180622-155238_imagenes_lv_terceros_emoyis-knNG-U453244591237BD-992x558@LaVanguardia-Web.JPG"),
+];
